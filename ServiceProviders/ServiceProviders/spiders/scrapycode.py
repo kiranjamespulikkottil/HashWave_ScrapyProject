d29eed988ce79172fd9801b62e5f244f04ed2b73
Kiran James <kiranjamespulikkottil@gmail.com>
2019-04-09 20:28:05 +0530

    Third Commit - UpdateTheCodeForSelectors

--- ServiceProviders/ServiceProviders/spiders/scrapycode.py
+++ ServiceProviders/ServiceProviders/spiders/scrapycode.py
@@ -4,10 +4,14 @@ class GlobaltradeSpider(scrapy.Spider):
     name = "globaltrade"
 
     start_urls = [
-            'https://www.globaltrade.net/United-States/expert-service-provider.html',
+            'https://www.globaltrade.net/expert-service-provider.html',
             ]
 
     def parse(self, response):
+
+        for href in response.css('.sp_country_71 ::attr(href)'):
+            yield response.follow(href, self.parse)
+
         for href in response.css('.sp-image a::attr(href)'):
             yield response.follow(href, self.parse_response)
 
@@ -16,5 +20,11 @@ class GlobaltradeSpider(scrapy.Spider):
         yield {
                 'log_url' : response.css('.image img::attr(data-original)').get(),
                 'title' : response.css('.sp-title span::text').get(),
+                'sub_title': response.css('span.sub ::text').get(),
+                'primary_location' : response.css('.profile-details td span::text')[1].get(),
+                'area_of_expertise' : response.css('.mainExp ::text').get(),
+                'about' : response.css('.section table p::text').getall(),
+                'website' : response.css('.section table tr a::text').get(),
+                'language_spoken' : response.css('.section table tr ::text').get(),
                 'page_url' : response.url,
             }
