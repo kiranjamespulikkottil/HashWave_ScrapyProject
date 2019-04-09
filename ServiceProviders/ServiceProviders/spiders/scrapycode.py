336df2963dcc6baf6a0dabf017c6508f1aa4463c
Kiran James <kiranjamespulikkottil@gmail.com>
2019-04-09 15:03:04 +0530

    Second commit - UpdateTheCode

--- ServiceProviders/ServiceProviders/spiders/scrapycode.py
+++ ServiceProviders/ServiceProviders/spiders/scrapycode.py
@@ -4,10 +4,15 @@ class GlobaltradeSpider(scrapy.Spider):
     name = "globaltrade"
 
     start_urls = [
-            'https://www.globaltrade.net/international-trade-import-exports/expert-service-provider-p/Fastfix-Inc.html',
+            'https://www.globaltrade.net/United-States/expert-service-provider.html',
             ]
 
     def parse(self, response):
+        for href in response.css('.sp-image a::attr(href)'):
+            yield response.follow(href, self.parse_response)
+
+
+    def parse_response(self, response):
         yield {
                 'log_url' : response.css('.image img::attr(data-original)').get(),
                 'title' : response.css('.sp-title span::text').get(),
